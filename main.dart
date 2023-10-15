2f5230fd08938c44678e3fd89d79246c84649ea9
guneshamza0 <145961431+guneshamza0@users.noreply.github.com>
2023-10-15 22:20:18 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,33 +1,130 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const flutter_uygulamalari());
-}
+void main() => runApp(MedicineReminder());
 
-class flutter_uygulamalari extends StatelessWidget {
-  const flutter_uygulamalari({super.key});
+class MedicineReminder extends StatelessWidget {
+  const MedicineReminder({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Sahibinden",
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("SAHİBİNDEN"),
-          leading: const Icon(Icons.menu),
-          backgroundColor: Colors.amberAccent,
-          centerTitle: true,
+          title: Text("Today's Medicine by Hamza Güneş"),
         ),
         body: Container(
-            color: Colors.amber,
-            child: Center(
-            child: Column(
-              children: [
-                Image.asset("assest/images/indir.jpg", height: 300,),
-                Text("GİRİŞ")
-              ],)
-            )),
+          padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.amber),
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.only(topLeft: Radius.circular(20),bottomLeft: Radius.circular(20)),
+                          color: Colors.purple),
+                      height: 70,
+                      width: 70,
+                      child: Icon(Icons.medical_information),
+                    ),
+                    Container(
+                      width: 500,
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Vitamin C"),
+                          Text(" * 2 Capsule"),
+                          Text(" * 3 times in a day"),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      padding: EdgeInsets.fromLTRB(100, 0, 0, 0),
+                      child: Text("7.30 AM"),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),color: Colors.amber
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(topLeft: Radius.circular(20),bottomLeft: Radius.circular(20)),
+                          color: Colors.green),
+                      height: 70,
+                      width: 70,
+                      child: Icon(Icons.medical_information),
+                    ),
+                    Container(
+                      width: 500,
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Aferin"),
+                          Text(" * 1 Capsule"),
+                          Text(" * 1 times in a day"),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      padding: EdgeInsets.fromLTRB(100, 0, 0, 0),
+                      child: Text("7.30 AM"),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20.0),
+                    color: Colors.amber),
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomLeft: Radius.circular(20)),
+                        color: Colors.red,
+                      ),
+                      height: 70,
+                      width: 70,
+                      child: Icon(Icons.medical_information),
+                    ),
+                    Container(
+                      width: 500,
+                      padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Sudofed"),
+                          Text(" * 2 Capsule"),
+                          Text(" * 3 times in a day"),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: 200,
+                      padding: EdgeInsets.fromLTRB(100, 0, 0, 0),
+                      child: Text("7.30 AM"),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
